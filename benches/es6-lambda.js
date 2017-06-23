4b62adfcd8875cbec14ff3a056700c6f8285b627
Venkat Peri <venkatperi@gmail.com>
2017-06-23 19:05:54 -0400

    bugs

--- benches/es6-lambda.js
+++ benches/es6-lambda.js
@@ -1,7 +1,10 @@
 suite( 'es6 lambda vs function', () => {
-  bench( 'lambda', () => 'Hello World!'.indexOf( 'o' ) > -1 );
-
-  bench( 'function', function () {
-    'Hello World!'.indexOf( 'o' ) > -1;
+  suite( 'sub suite 1', () => {
+    bench( 'lambda', () => 'Hello World!'.indexOf( 'o' ) > -1 );
+    bench( 'function', function () { let x = 'Hello World!'.indexOf( 'o' ) > -1; } );
+  } );
+  suite( 'sub suite 2', () => {
+    bench( 'lambda', () => 'Hello World!'.indexOf( 'o' ) > -1 );
+    bench( 'function', function () { let x = 'Hello World!'.indexOf( 'o' ) > -1; } );
   } );
 } );
